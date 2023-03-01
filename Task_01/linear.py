d051775fc27ef956babf9bd660e7b111aab088b2
Oleh Plakyda <gamermrgelo@gmail.com>
2023-03-01 23:17:59 +0200

    Completed task

--- Task_01/linear.py
+++ Task_01/linear.py
@@ -1,20 +1,21 @@
-def encrypt_caesar(plaintext, shift):
+def encrypt_linear(plaintext, key):
     """
-    Функція для шифрування тексту шифром Цезаря з ключем shift
+    Функція для шифрування тексту Лінійним шифром з ключем key
     """
     ciphertext = ''
     small_symbols = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
     big_symbols = 'АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
+    l_a = len(big_symbols)
     for char in plaintext:
         if char.isalpha():
             # Визначаємо номер символа в алфавіті
-            # Зсуваємо символ на shift позицій
+            # Множимо номер на ключ та знаходимо символ
             if char.islower():
-                symbol_number = small_symbols.find(char)
-                shifted_symbol = small_symbols[(symbol_number + shift) % len(small_symbols)]
+                symbol_number = small_symbols.find(char) + 1
+                shifted_symbol = small_symbols[(symbol_number * key) % l_a]
             else:
-                symbol_number = big_symbols.find(char)
-                shifted_symbol = big_symbols[(symbol_number + shift) % len(big_symbols)]
+                symbol_number = big_symbols.find(char) + 1
+                shifted_symbol = big_symbols[(symbol_number * key) % l_a]
             # Додаємо символ до шифрованого тексту
             ciphertext += shifted_symbol
         else:
@@ -23,24 +24,26 @@ def encrypt_caesar(plaintext, shift):
     return ciphertext
 
 
-def decrypt_caesar(ciphertext, shift):
+def decrypt_linear(ciphertext, key):
     """
-    Функція для розшифрування тексту, зашифрованого шифром Цезаря з ключем shift
+    Функція для розшифрування тексту, зашифрованого Лінійним шифром з ключем key
     """
     plaintext = ''
     small_symbols = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
     big_symbols = 'АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
+    l_a = len(big_symbols)
+    special_key = key**(l_a-4) % l_a
     for char in ciphertext:
         if char.isalpha():
             # Визначаємо номер символа в алфавіті
-            # Зсуваємо символ на shift позицій
+            # Зміщуемо за допомогою спеціального ключа знайденого за формулою
             if char.islower():
                 symbol_number = small_symbols.find(char)
-                shifted_symbol = small_symbols[(symbol_number - shift) % len(small_symbols)]
+                shifted_symbol = small_symbols[symbol_number * special_key % l_a - 1]
             else:
                 symbol_number = big_symbols.find(char)
-                shifted_symbol = big_symbols[(symbol_number - shift) % len(big_symbols)]
-            # Додаємо символ до шифрованого тексту
+                shifted_symbol = big_symbols[symbol_number * special_key % l_a - 1]
+            # Додаємо символ до розшифрованого тексту
             plaintext += shifted_symbol
         else:
             # Додаємо символ без змін
@@ -49,11 +52,11 @@ def decrypt_caesar(ciphertext, shift):
 
 
 def main():
-    shift = 4
+    key = 5
     plaintext = 'Я, Плакида Олег Миколайович, студент університету.'
-    encrypt_text = encrypt_caesar(plaintext, shift)
+    encrypt_text = encrypt_linear(plaintext, key)
     print(encrypt_text)
-    print(decrypt_caesar(encrypt_text, shift))
+    print(decrypt_linear(encrypt_text, key))
 
 
 main()
